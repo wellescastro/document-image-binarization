207fa846bddcac1f6d2f07d07f5c0bc9c73d6c44
Dayvid Welles <wellescastro@gmail.com>
2018-10-18 16:12:57 -0300

    adding transforms

--- util/DIBCOReader.py
+++ util/DIBCOReader.py
@@ -4,6 +4,8 @@ from glob import glob
 import cv2
 import numpy as np
 from skimage.util.shape import view_as_blocks
+import random
+from PIL import Image
 
 class DIBCODataset(Dataset):
 
@@ -17,7 +19,7 @@ class DIBCODataset(Dataset):
         2016: ['handwritten']
     }
 
-    def __init__(self, basepath="/home/dayvidwelles/phd/code/computer-vision-project/data/Dibco", years=[2009,2010,2011,2012,2013,2014], transforms=None, windows_size=(256,256)):
+    def __init__(self, basepath="/home/dayvidwelles/phd/code/computer-vision-project/data/Dibco", years=[2009,2010,2011,2012,2013,2014], transform=None, windows_size=(256,256)):
         data_files = []
         for year in years:
             for subset in self.DIBCO[year]:
@@ -42,12 +44,14 @@ class DIBCODataset(Dataset):
             img_gr = cv2.resize(img_gr, (new_gr_w, new_gr_h), interpolation = cv2.INTER_CUBIC)
             img_gt = cv2.resize(img_gt, (new_gr_w, new_gr_h), interpolation = cv2.INTER_CUBIC)
 
-            img_gr_patches = view_as_blocks(img_gr, windows_size).reshape(-1, windows_size[0], windows_size[1])
-            img_gt_patches = view_as_blocks(img_gt, windows_size).reshape(-1, windows_size[0], windows_size[1])
+            # img_gr_patches = view_as_blocks(img_gr, windows_size).reshape(-1, windows_size[0], windows_size[1])
+            # img_gt_patches = view_as_blocks(img_gt, windows_size).reshape(-1, windows_size[0], windows_size[1])
+            img_gr_patches = self.blockshaped(img_gr, windows_size[0], windows_size[1])
+            img_gt_patches = self.blockshaped(img_gt, windows_size[0], windows_size[1])
 
             X_train.extend(img_gr_patches)
             Y_train.extend(img_gt_patches)
-
+            
         # convert to arrays
         X_train = np.asarray(X_train).astype('float32')
         Y_train = np.asarray(Y_train).astype('float32')
@@ -61,26 +65,48 @@ class DIBCODataset(Dataset):
 
         self.X_train = X_train
         self.Y_train = Y_train
+        self.transform = transform
+        self.target_transform = transform
 
-    def patchify(self, img, patch_shape):
-        img = np.ascontiguousarray(img)  # won't make a copy if not needed
-        X, Y = img.shape
-        x, y = patch_shape
-        shape = ((X-x+1), (Y-y+1), x, y) # number of patches, patch_shape
-        # The right strides can be thought by:
-        # 1) Thinking of `img` as a chunk of memory in C order
-        # 2) Asking how many items through that chunk of memory are needed when indices
-        #    i,j,k,l are incremented by one
-        strides = img.itemsize*np.array([Y, 1, Y, 1])
-        return np.lib.stride_tricks.as_strided(img, shape=shape, strides=strides)
+    def blockshaped(self, arr, nrows, ncols):
+        """
+        Return an array of shape (n, nrows, ncols) where
+        n * nrows * ncols = arr.size
 
+        If arr is a 2D array, the returned array looks like n subblocks with
+        each subblock preserving the "physical" layout of arr.
+        """
+        h, w = arr.shape
+        return (arr.reshape(h//nrows, nrows, -1, ncols)
+                .swapaxes(1,2)
+                .reshape(-1, nrows, ncols))
+
+
+    def unblockshaped(self, arr, h, w):
+        """
+        Return an array of shape (h, w) where
+        h * w = arr.size
+
+        If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
+        then the returned array preserves the "physical" layout of the sublocks.
+        """
+        n, nrows, ncols = arr.shape
+        return (arr.reshape(h//nrows, -1, nrows, ncols)
+                .swapaxes(1,2)
+                .reshape(h, w))
     
     def __getitem__(self, index):
-        filename_gr = self.data_files[index]
-        filename_gt = self.data_files[index].replace("GR", "GT")
-
-        img_gr = cv2.imread(filename_gr, cv2.IMREAD_GRAYSCALE)
-        img_gt = cv2.imread(filename_gt, cv2.IMREAD_GRAYSCALE)
+        img_gr = Image.fromarray(self.X_train[index])
+        img_gt = Image.fromarray(self.Y_train[index])
+        
+        seed = np.random.randint(2147483647) # make a seed with numpy generator 
+        random.seed(seed) # apply this seed to img tranfsorms
+        if self.transform is not None:
+            img_gr = self.transform(img_gr)
+        
+        random.seed(seed) # apply this seed to target tranfsorms
+        if self.target_transform is not None:
+            img_gt = self.target_transform(img_gt)
 
         return (img_gr, img_gt)    
 
@@ -99,8 +125,8 @@ class DIBCODataset(Dataset):
 
 if __name__ == '__main__':
     # Define transforms (1)
-    transformations = transforms.Compose([transforms.CenterCrop(100), transforms.ToTensor()])
+    transformations = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
     # Call the dataset
     # custom_dataset = MyCustomDataset(..., transformations)
-    data_loader = DIBCODataset()
+    data_loader = DIBCODataset(transform=transformations)
     data_loader.__getitem__(0)
