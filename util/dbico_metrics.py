437d750d5b4dee0e1f80536d84227f0df6798348
Dayvid Welles <wellescastro@gmail.com>
2018-10-20 22:18:02 -0300

    dibco metrics adjusted

--- util/dbico_metrics.py
+++ util/dbico_metrics.py
@@ -32,7 +32,7 @@ def drd_fn(im, im_gt):
 	m = n*2+1
 	W = np.zeros((m,m), dtype=np.uint8)
 	W[n,n] = 1.
-	W = cv2.distanceTransform(1-W, cv2.cv.CV_DIST_L2, cv2.cv.CV_DIST_MASK_PRECISE)
+	W = cv2.distanceTransform(1-W, cv2.DIST_L2, cv2.DIST_MASK_PRECISE)
 	W[n,n] = 1.
 	W = 1./W
 	W[n,n] = 0.
@@ -70,6 +70,75 @@ def drd_fn(im, im_gt):
 
 		drd_sum += np.sum(tmp)
 	return drd_sum/nubn
+
+
+def compute_metrics(im, im_gt):
+	height, width = im.shape
+	npixel = height*width
+
+	im[im>0] = 1
+	gt_mask = im_gt==0
+	im_gt[im_gt>0] = 1
+
+	sk = bwmorph(1-im_gt)
+	im_sk = np.ones(im_gt.shape)
+	im_sk[sk] = 0
+	
+	kernel = np.ones((3,3), dtype=np.uint8)
+	im_dil = cv2.erode(im_gt, kernel)
+	im_gtb = im_gt-im_dil
+	im_gtbd = cv2.distanceTransform(1-im_gtb, cv2.DIST_L2, 3)
+	
+	nd = im_gtbd.sum()
+
+	ptp = np.zeros(im_gt.shape)
+	ptp[(im==0) & (im_sk==0)] = 1
+	numptp = ptp.sum()
+
+	tp = np.zeros(im_gt.shape)
+	tp[(im==0) & (im_gt==0)] = 1
+	numtp = tp.sum()
+
+	tn = np.zeros(im_gt.shape)
+	tn[(im==1) & (im_gt==1)] = 1
+	numtn = tn.sum()
+
+	fp = np.zeros(im_gt.shape)
+	fp[(im==0) & (im_gt==1)] = 1
+	numfp = fp.sum()
+
+	fn = np.zeros(im_gt.shape)
+	fn[(im==1) & (im_gt==0)] = 1
+	numfn = fn.sum()
+
+	precision = numtp / (numtp + numfp)
+	recall = numtp / (numtp + numfn)
+	precall = numptp / np.sum(1-im_sk)
+	fmeasure = (2*recall*precision)/(recall+precision)
+	pfmeasure = (2*precall*precision)/(precall+precision)
+
+	mse = (numfp+numfn)/npixel
+	psnr = 10.*np.log10(1./mse)
+
+	nrfn = numfn / (numfn + numtp)
+	nrfp = numfp / (numfp + numtn)
+	nrm = (nrfn + nrfp)/2
+
+	im_dn = im_gtbd.copy()
+	im_dn[fn==0] = 0
+	dn = np.sum(im_dn)
+	mpfn = dn / nd
+
+	im_dp = im_gtbd.copy()
+	im_dp[fp==0] = 0;
+	dp = np.sum(im_dp)
+	mpfp = dp / nd
+
+	mpm = (mpfp + mpfn) / 2
+	drd = drd_fn(im, im_gt)
+
+	# print "F-measure\t: {0}\npF-measure\t: {1}\nPSNR\t\t: {2}\nNRM\t\t: {3}\nMPM\t\t: {4}\nDRD\t\t: {5}".format(fmeasure, pfmeasure, psnr, nrm, mpm, drd)
+	return (fmeasure, pfmeasure, psnr, nrm, mpm, drd)
 
 if __name__=="__main__":
 	if len(sys.argv)<3:
