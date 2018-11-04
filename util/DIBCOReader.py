2c66a292e981f452f277aeb4d88a828088ce686d
Dayvid Welles <wellescastro@gmail.com>
2018-11-03 23:50:36 -0300

    clean up 2

--- util/DIBCOReader.py
+++ util/DIBCOReader.py
@@ -103,34 +103,6 @@ class DIBCODataset(Dataset):
         self.transform = transform
         self.target_transform = transform
         self.list_of_patches = list_of_patches
-
-
-    def blockshaped(self, arr, nrows, ncols):
-        """
-        Return an array of shape (n, nrows, ncols) where
-        n * nrows * ncols = arr.size
-
-        If arr is a 2D array, the returned array looks like n subblocks with
-        each subblock preserving the "physical" layout of arr.
-        """
-        h, w = arr.shape
-        return (arr.reshape(h//nrows, nrows, -1, ncols)
-                .swapaxes(1,2)
-                .reshape(-1, nrows, ncols))
-
-
-    def unblockshaped(self, arr, h, w):
-        """
-        Return an array of shape (h, w) where
-        h * w = arr.size
-
-        If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
-        then the returned array preserves the "physical" layout of the sublocks.
-        """
-        n, nrows, ncols = arr.shape
-        return (arr.reshape(h//nrows, -1, nrows, ncols)
-                .swapaxes(1,2)
-                .reshape(h, w))
     
 
     def __getitem__(self, index):
