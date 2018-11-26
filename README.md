17ec012a54decf506409f616ca43c5cdb6d58c42
Dayvid Welles <wellescastro@gmail.com>
2018-11-25 23:05:30 -0300

    last changes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 computer-vision-project
+
+
+# Training
+
+python train.py
+
+
+# Evaluation
+
+python eval.py --model weights/DIBCO2016-WHOLE_AUG/best_model.pth --year 2016
+
+
+# Binarization
+
+python binarize.py --model weights/DIBCO2014-WHOLE_AUG/best_model.pth --image_path data/Dibco/2016/handwritten_GR/10.png
--- augmentations.sh
+++ augmentations.sh
@@ -1,11 +1,11 @@
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2009/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2009/handwritten_GR_aug
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2010/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2010/handwritten_GR_aug
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2011/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2011/handwritten_GR_aug
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2012/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2012/handwritten_GR_aug 
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2013/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2013/handwritten_GR_aug 
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2014/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2014/handwritten_GR_aug
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2016/handwritten_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2016/handwritten_GR_aug
+python py_augmentation.py -path data/Dibco/2009/handwritten_GR -out data/Dibco/2009/handwritten_GR_aug
+python py_augmentation.py -path data/Dibco/2010/handwritten_GR -out data/Dibco/2010/handwritten_GR_aug
+python py_augmentation.py -path data/Dibco/2011/handwritten_GR -out data/Dibco/2011/handwritten_GR_aug
+python py_augmentation.py -path data/Dibco/2012/handwritten_GR -out data/Dibco/2012/handwritten_GR_aug 
+python py_augmentation.py -path data/Dibco/2013/handwritten_GR -out data/Dibco/2013/handwritten_GR_aug 
+python py_augmentation.py -path data/Dibco/2014/handwritten_GR -out data/Dibco/2014/handwritten_GR_aug
+python py_augmentation.py -path data/Dibco/2016/handwritten_GR -out data/Dibco/2016/handwritten_GR_aug
 
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2009/printed_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2009/printed_GR_aug
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2011/printed_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2011/printed_GR_aug
-python py_augmentation.py -path /home/dayvidwelles/phd/code/cv-project/data/Dibco/2013/printed_GR -out /home/dayvidwelles/phd/code/cv-project/data/Dibco/2013/printed_GR_aug 
+python py_augmentation.py -path data/Dibco/2009/printed_GR -out data/Dibco/2009/printed_GR_aug
+python py_augmentation.py -path data/Dibco/2011/printed_GR -out data/Dibco/2011/printed_GR_aug
+python py_augmentation.py -path data/Dibco/2013/printed_GR -out data/Dibco/2013/printed_GR_aug 
