b6e72f97f11a4db8725586bc320789b072824cc9
Dayvid Welles <wellescastro@gmail.com>
2018-11-25 22:30:56 -0300

    unecessary eps

--- util/metrics.py
+++ util/metrics.py
@@ -16,10 +16,10 @@ def paper_f1score(logits, targets, beta, eps=1e-9):
     beta2 = beta ** 2.0
     logits = logits.float()
     targets = targets.float()
-    top = torch.mul(targets, logits).sum().add(eps)
+    top = torch.mul(targets, logits).sum()
     bot = beta2 * targets.sum() + logits.sum().add(eps)
     return torch.mean((1.0 + beta2) * top / bot)
-
+    
 
 def fbeta_score(y_true, y_pred, beta, threshold, eps=1e-9):
     beta2 = beta**2
@@ -112,5 +112,5 @@ py_true = torch.from_numpy(y_true).view(-1)
 fbeta_pytorch = f_score(py_true, py_pred)
 fbeta_sklearn = metrics.fbeta_score(y_true, y_pred, 1, average='micro')
 
-print('Scores are {:.5f} (sklearn) and {:.5f} (pytorch)'.format(fbeta_sklearn, fbeta_pytorch))
-print("{:.5f}".format(paper_f1score(py_true, py_pred, 1)))
+# print('Scores are {:.5f} (sklearn) and {:.5f} (pytorch)'.format(fbeta_sklearn, fbeta_pytorch))
+# print("{:.5f}".format(paper_f1score(py_true, py_pred, 1)))
