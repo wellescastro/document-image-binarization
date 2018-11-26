785786a6823ccd4fe4813f0dbed6aad2a4a7688f
Dayvid Welles <wellescastro@gmail.com>
2018-11-25 22:54:26 -0300

    final adjustments

--- eval.py
+++ eval.py
@@ -12,7 +12,6 @@ from util.DIBCOReader import DIBCODataset
 from torchvision import transforms
 from models.AutoEncoder import AutoEncoder
 from util import losses
-from EarlyStopping import EarlyStopping
 from util.metrics import f_score, mse_score, f_score_no_threshold
 from util.utils import save_checkpoint, load_checkpoint, load_weights
 from skimage.util.shape import view_as_blocks
@@ -23,7 +22,7 @@ from util.dbico_metrics import compute_metrics
 from skimage.transform import warp, AffineTransform
 import time
 import torch.backends.cudnn as cudnn
-from loss import LossBinary
+import argparse
 
 def criterion(logits, labels):
     # return losses.FBeta_ScoreLoss().forward(logits, labels)
@@ -55,63 +54,21 @@ class ToTensorNoScale(object):
             return img.float()
         return img
 
-def main():
+def main(model_filename, year, save_dir):
     # Hyperparameters
-    batch_size = 25
-    epochs = 300
     threshold = 0.5
-    early_stopping_patience = 20
     window_size = 256,256
-    strides = 128,128
+    strides = 96,96
 
-    # Training informartion
-    model_weiths_path = "checkpoints/"
-    resume_training = False
-    if resume_training is False:
-        start_epoch = 0
-    else:
-        start_epoch = 10
-    model_name = "auto_encoder_aug5"
-    resume_checkpoint = model_weiths_path + "{}-epoch-{}.pth".format(model_name, start_epoch)
-
-    use_cuda = torch.cuda.is_available()
+    if save_dir is not None and os.path.exists(save_dir) is False:
+        os.makedirs(save_dir)
 
     # define the model and optimizer
     net = AutoEncoder(nb_layers=5).cuda()
-    print(net)
+    print("Initializing evaluation of model {}...".format(model_filename))
     optimizer = optim.Adam(net.parameters(), lr=0.001)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, verbose=True)
-  
-    # define the dataset and data augmentation operations
-    training_transforms = transforms.Compose([
-                # transforms.ToPILImage(mode='L'),
-                # # transforms.RandomResizedCrop(window_size[0], scale=(0.08, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
-                transforms.RandomAffine(degrees=0, scale=(0.5,1.5)),
-                # # RandomAffineTransform(scale_range=(0.5,1.5),rotation_range=(0,0),shear_range=(0,0), translation_range=(0,0)),
-                # transforms.RandomHorizontalFlip(),
-                # transforms.RandomVerticalFlip(),
-                # ToTensorNoScale(),
-                transforms.ToTensor()
-                # transforms.Normalize(mean=[0.5], std=[0.5])
-                ])
-
-    training_target_transforms = transforms.Compose([
-                # transforms.ToPILImage(mode='L'),
-                # # transforms.RandomResizedCrop(window_size[0], scale=(0.08, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
-                transforms.RandomAffine(degrees=0, scale=(0.5,1.5)),
-                # # RandomAffineTransform(scale_range=(0.5,1.5),rotation_range=(0,0),shear_range=(0,0), translation_range=(0,0)),
-                # transforms.RandomHorizontalFlip(), 
-                # transforms.RandomVerticalFlip(),
-                # # ToTensorNoScale(),
-                transforms.ToTensor(),
-                # transforms.Normalize(mean=[0.5], std=[0.5])
-                ])
-
-
-    training_set = DIBCODataset(years=[2009, 2010, 2011, 2012, 2013, 2014],
-    transform = training_transforms, target_transform=training_target_transforms, window_size=window_size, stride=strides, include_augmentation=False
-    )
-
+ 
     testing_transforms = transforms.Compose([ 
         # ToTensorNoScale(),
         transforms.ToTensor()
@@ -121,41 +78,17 @@ def main():
         transforms.ToTensor()
     ])
 
-    testing_set = DIBCODataset(years=[2016], 
+    testing_set = DIBCODataset(years=[year], 
     transform = testing_transforms, target_transform=testing_target_transforms, window_size=window_size, stride=strides, include_augmentation=False
     )
 
-    train_loader = DataLoader(training_set,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=4,
-                          pin_memory=True # CUDA only
-                         )
-    
-    test_loader = DataLoader(testing_set,
-                          batch_size=batch_size,
-                          shuffle=False,
-                          num_workers=4,
-                          pin_memory=True # CUDA only
-                         )
+    load_weights(net, model_filename)
+    f1, pf1 = final_evaluation(net, testing_set, testing_transforms, window_size, strides, threshold, save_dir=save_dir)
+    print("Results: F1-Score: {:4f} Pseudo F1-Score: {:4f}\n".format(f1, pf1))
 
-    print("Training on {} samples and testing on {} samples "
-          .format(len(train_loader.dataset), len(test_loader.dataset)))
-
-    early_stopper = EarlyStopping(mode='min', patience=early_stopping_patience)
-
-    # optionally resume from a checkpoint
-    if resume_training:
-        start_epoch, early_stopper.best, early_stopper.num_bad_epochs = load_checkpoint(net, optimizer, resume_checkpoint)
-    cudnn.benchmark = True
-
-    load_weights(net, "checkpoints/model_best.pth")
-
-    print(final_evaluation(net, testing_set, testing_transforms, window_size, strides, threshold))
-
-def final_evaluation(net, testing_set, testing_transforms, window_size, strides, threshold):
+def final_evaluation(net, testing_set, testing_transforms, window_size, strides, threshold, save_dir=None):
     fmeasures = []
-    sk_fmeasures = []
+    pfmeasures = []
     scaler = MinMaxScaler(feature_range=(0,255))
     net.eval()
     with torch.no_grad():
@@ -168,13 +101,14 @@ def final_evaluation(net, testing_set, testing_transforms, window_size, strides,
             #preprocessing
             img_gr = 255 - img_gr
 
-            img_prediction = np.zeros(img_gr.shape, dtype=np.uint8)
+            count_strides = np.ones(img_gr.shape, dtype=int)
+            img_prediction = np.zeros(img_gr.shape, dtype=np.float64)
             for coords, patch in sliding_window(img_gr, strides, window_size):
                 patch_size = patch.shape
 
                 vertical_padding = (window_size[0] - patch_size[0])
                 horizontal_padding = (window_size[1] - patch_size[1])
-                patch_gr = np.pad(patch, ((0,vertical_padding),(0,horizontal_padding)),mode='constant')
+                patch_gr = np.pad(patch, ((0, vertical_padding), (0, horizontal_padding)),mode='constant')
                 patch_gr_with_channel = np.expand_dims(patch_gr, 2) # change to 0 if using final transforms
     
                 inputs = testing_transforms(patch_gr_with_channel).cuda()
@@ -182,37 +116,68 @@ def final_evaluation(net, testing_set, testing_transforms, window_size, strides,
 
                 # forward
                 logits = net(inputs)
-                pred = (logits > 0.5).float() # threshold the output activation map
+                # pred = (logits > 0.5).float() # threshold the output activation map
+                logits.float()
                 pred = logits.squeeze() # remove all dimensions of size 1 (batch dimension and channel dimension)
-                
+                pred = pred.data.cpu().numpy()
+
                 prediction_unpadded = pred[0:window_size[0]-vertical_padding, 0:window_size[1]-horizontal_padding]
-                img_prediction[coords[0]:coords[1],coords[2]:coords[3]] = prediction_unpadded
+                prediction_unpadded = prediction_unpadded.astype(np.float64)
+                # print(prediction_unpadded[0,128])
+
+                patch_predicted = img_prediction[coords[0]:coords[1],coords[2]:coords[3]]
+
+                # for i in range(patch_predicted.shape[0]):
+                #     for j in range(patch_predicted.shape[1]):
+                #         if patch_predicted[i, j] > 0:
+                #             count_strides[coords[0]+i,coords[2]+j] += 1
+
+                idxs = list(np.nonzero(patch_predicted))
+                if len(idxs[0]) > 0:
+                    idxs[0] += coords[0]
+                    idxs[1] += coords[2]
+                    count_strides[idxs] += 1
                 
+                img_prediction[coords[0]:coords[1],coords[2]:coords[3]] = img_prediction[coords[0]:coords[1],coords[2]:coords[3]] + prediction_unpadded + 1e-30
+                
+            img_prediction /= count_strides # disable if do not work
+            img_prediction = img_prediction > 0.5
             img_prediction = 255 * img_prediction
             img_prediction = 255 - img_prediction
+            img_prediction = img_prediction.astype(np.uint8)
+
+            if save_dir is not None:
+                Image.fromarray(img_prediction).save(save_dir + "/" + filename_gr.split("/")[-1])
             
             fmeasure, pfmeasure, psnr, nrm, mpm, drd = compute_metrics(img_prediction.copy(), img_gt.copy())
             fmeasures.append(fmeasure)
+            pfmeasures.append(pfmeasure)
 
-            sk_fmeasures.append(sk_metrics.fbeta_score( ((255 - img_gt.ravel()) / 255), ((255 - img_prediction.ravel()) / 255), 1))
-        
-        return np.mean(fmeasures)
+        return np.mean(fmeasures), np.mean(pfmeasure)
         
 
 def sliding_window(img, strides, window_size):
     shape = img.shape
-    tiles_per_row = int( np.ceil( shape[0] / float(strides[0]) ) )
-    tiles_per_col = int( np.ceil( shape[1] / float(strides[1]) ) )
+    tiles_row_wise = int( np.ceil( shape[0] / float(strides[0]) ) )
+    tiles_col_wise = int( np.ceil( shape[1] / float(strides[1]) ) )
 
-    for i in range(tiles_per_row):
-        for j in range(tiles_per_col):
+    for i in range(tiles_row_wise):
+        for j in range(tiles_col_wise):
             i_0 = strides[0]*i
             i_f = i_0 + window_size[0]
 
             j_0 = strides[1]*j
             j_f = j_0 + window_size[1]
+
+            if i_f > img.shape[0]:
+                i_f = img.shape[0]
+     
             
-            yield (i_0,i_f, j_0,j_f), img[i_0:i_f, j_0:j_f]
+            if j_f > img.shape[1]:
+                j_f = img.shape[1] 
+     
+            
+            yield (i_0, i_f, j_0, j_f), img[i_0:i_f, j_0:j_f]
 
 
 if __name__ == '__main__':
@@ -222,5 +187,15 @@ if __name__ == '__main__':
     # parser.add_argument("--batch_size",type=int,default=1,help="batch size")
     # parser.add_argument("--num_gpu",type=int,default=1,help="number of gpus")
     # args = parser.parse_args()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model",type=str,help="please specify the model to load")
+    parser.add_argument("--year",type=int,help="please specify the dibco year to test")
+    parser.add_argument("--save_dir", type=str, help="in case you want save the image results")
+    args = parser.parse_args()
+    
+    model_filename = args.model
+    year = args.year
+    save_dir = args.save_dir
 
-    main()
+    main(model_filename, year, save_dir)
